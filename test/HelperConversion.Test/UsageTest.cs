06e71e243792f141c7533ba0c1931ce2245de562
leovene <leo.ochoalopes@gmail.com>
2017-08-17 17:45:35 -0300

    Unit date helper test

---
+++ test/HelperConversion.Test/UsageTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HelperConversion.Test
+{
+    [TestClass]
+    public class UsageTest
+    {
+        [TestMethod]
+        [TestCategory("Date Helper")]
+        public void GiveAValidBirthdateReturnAge()
+        {
+            DateTime date = new DateTime(1984, 11, 15);
+            Assert.IsTrue(date.Age() >= 32);
+        }
+
+        [TestMethod]
+        [TestCategory("Date Helper")]
+        public void GiveAInvalidBirthdateReturnNegativeAge()
+        {
+            DateTime date = new DateTime(DateTime.Now.Year+15, 11, 15);
+            Assert.IsTrue(date.Age() < 0);
+        }
+
+        [TestMethod]
+        [TestCategory("Date Helper")]
+        public void GiveATodayBirthdateReturnZeroAge()
+        {
+            DateTime date = DateTime.Now;
+            Assert.IsTrue(date.Age() == 0);
+        }
+    }
+}
