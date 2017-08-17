e2f2cb2f28628e14c30c3fb8d8922b079dcbdbb7
leovene <leo.ochoalopes@gmail.com>
2017-08-17 14:40:27 -0300

    DateHelper

---
+++ src/HelperConversion/DateHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelperConversion
+{
+    public static class DateHelper
+    {
+        public static int Age(this DateTime birthdate)
+        {
+            int years = DateTime.Now.Year - birthdate.Year;
+            if ((birthdate.Month > DateTime.Now.Month) || (birthdate.Month == DateTime.Now.Month && birthdate.Day > DateTime.Now.Day))
+                years--;
+            return years;
+        }
+    }
+}
---
+++ test/HelperConversion.Test/DateHelperTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HelperConversion.Test
+{
+    [TestClass]
+    public class DateHelperTest
+    {
+        [TestMethod]
+        [TestCategory("Age")]
+        public void GiveAValidYearsReturnAge()
+        {
+            DateTime year = DateTime.Now;
+            Assert.Equals(33,year.Age());
+        }
+    }
+}
